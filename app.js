703bd8a386def7d8dfa1f6ff1f1d4d1703b8bb63
jenna-graham <jclayto2@gmail.com>
2022-04-28 11:04:33 -0700

    added display catchphrase function

--- app.js
+++ app.js
@@ -17,12 +17,12 @@ let middleCount = 0;
 let bottomCount = 0;
 
 // set state for all of the character's catchphrases
-let catchphrasesCount = [];
+let catchphrases = [];
 
 
 headDropdown.addEventListener('change', () => {
     // get the value of the head dropdown
-    console.log('changing head selection');
+    console.log('changing head selection', headDropdown.value);
     // increment the head change count state
     headCount++; 
     // update the dom for the head (use style.backgroundImage on the headEl div instead of trying to set the .src -- it's NOT an img tag!)
@@ -34,7 +34,7 @@ headDropdown.addEventListener('change', () => {
 
 middleDropdown.addEventListener('change', () => {
     // get the value of the middle dropdown
-    console.log('changing middle selection');
+    console.log('changing middle selection', middleDropdown.value);
     // increment the middle change count state
     middleCount++;
     // update the dom for the middle (NOTE: use style.backgroundImage on the middleEl div instead of trying to set the .src -- it's NOT an img tag!)
@@ -46,7 +46,7 @@ middleDropdown.addEventListener('change', () => {
 
 bottomDropdown.addEventListener('change', () => {
     // get the value of the bottom dropdown
-    console.log('changing bottom selection');
+    console.log('changing bottom selection', bottomDropdown.value);
     // increment the bottom change count state
     bottomCount++;
     // update the dom for the bottom (NOTE use style.backgroundImage on the bottomEl div instead of trying to set the .src -- it's NOT an img tag!)
@@ -60,12 +60,11 @@ catchphraseButton.addEventListener('click', () => {
     // get the value of the catchphrase input
     console.log(catchphraseInput.value);
     // push the new catchphrase to the catchphrase array in state
-    catchphrasesCount.push(catchphrasesEl.value);
-    console.log(catchphrasesCount);
+    catchphrases.push(catchphraseInput.value);
     // clear out the form input's value so it's empty to the user
     catchphraseInput.value = '';
     // update the dom to show the new catchphrases (refactor to/call displayCatchphrases to do this work)
-    
+    displayCatchphrases();
 });
 
 function displayStats() {
@@ -74,13 +73,18 @@ function displayStats() {
 }
 
 function displayCatchphrases() {
+    
     // clear out the DOM for the currently displayed catchphrases
-
+    catchphrasesEl.textContent = '';
     // loop through each catchphrase in state
-   
-    // and for each catchphrase
-    
-    // create an HTML element with the catchphrase as its text content
-    
-    // and append that HTML element to the cleared-out DOM
+     // and for each catchphrase
+    for (let catchphraseText of catchphrases) {
+        console.log(catchphrases);
+        const phrase = document.createElement('div');
+        // create an HTML element with the catchphrase as its text content
+        phrase.textContent = catchphraseText;
+        // and append that HTML element to the cleared-out DOM
+        catchphrasesEl.append(phrase);
+        
+    }   
 }
